9e174827ce74e8441b5b3641036b4a0f6f6824be
OnjaMiarinomena <onjafinaritramiarinomena@gmail.com>
2026-06-12 07:14:26 +0200

    updat all

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const roleCards = document.querySelectorAll(".role-card");
+/*pop up entre le connexion et l'inscription */
+const connexion_btn = document.getElementById("connexionBtn");
+const inscription_btn = document.getElementById("inscriptionBtn");
+const login_form_container = document.getElementById("LoginFormContainer");
+const register_form_container = document.getElementById(
+  "RegisterFormContainer",
+);
 
-roleCards.forEach((card) => {
-  card.addEventListener("click", () => {
-    roleCards.forEach((item) => {
-      item.classList.remove("active-role");
-    });
-
-    card.classList.add("active-role");
-  });
+connexion_btn.addEventListener("click", () => {
+  login_form_container.style.display = "block";
+  register_form_container.style.display = "none";
 });
+
+inscription_btn.addEventListener("click", () => {
+  login_form_container.style.display = "none";
+  register_form_container.style.display = "block";
+});
+/* le pop up entre le se conecter et deja une compte*/
